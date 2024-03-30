94885daaf4f3a365e874da3b37e04549d885a66b
Hoang130203 <k58a01.mmh@gmail.com>
2024-03-30 23:03:00 +0700

    .

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -4,20 +4,12 @@ import GoogleProvider from 'next-auth/providers/google'
 import FacebookProvider from 'next-auth/providers/facebook'
 import GithubProvider from 'next-auth/providers/github'
 import TwitterProvider from 'next-auth/providers/twitter'
-// # GOOGLE_CLIENT_ID=832403866831-dfhnhc4q08lcckhtv15ijilob1uhmlr6.apps.googleusercontent.com
-// # GOOGLE_CLIENT_SECRET=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
-// # # SECRET="ANYTEXT"
-// # FACEBOOK_CLIENT_ID=958478172526362
-// # FACEBOOK_CLIENT_SECRET=981b017d64823377477dceeae237f8bc
-// # GITHUB_CLIENT_ID=35cfb99df1b994a5625e
-// # GITHUB_CLIENT_SECRET=cc7312d5d47f05b1be9bb390898a3687aef9bf98
-// # TWITTER_CLIENT_ID=CI4nQzD5dQGxs7jha6svtYqPi
-// # TWITTER_CLIENT_SECRET=khsUO0Cm0W97MeKR3XWQrYuKqWRDhxvgR1iX8lhSMQ6d2Uv0j6
+
 const handler = NextAuth({
     providers: [
         GoogleProvider({
-            clientId: 832403866831 - dfhnhc4q08lcckhtv15ijilob1uhmlr6.apps.googleusercontent.com || "",
-            clientSecret: process.GOCSPX - d7BOezndK0KVMLFGvRvprr - v - d2I || ""
+            clientId: process.env.GOOGLE_CLIENT_ID || "",
+            clientSecret: process.env.GOOGLE_CLIENT_SECRET || ""
         }),
         FacebookProvider({
             clientId: process.env.FACEBOOK_CLIENT_ID,
