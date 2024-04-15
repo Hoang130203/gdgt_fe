9f5ce1d630280bcf60fe98b8c89e5efece894634
Hoang130203 <k58a01.mmh@gmail.com>
2024-04-15 22:28:46 +0700

    .

--- app/documents/page.js
+++ app/documents/page.js
@@ -3,15 +3,20 @@ function Document() {
     // PRJ2FLIX
 
     return (
-        <div style={{ minHeight: '800px', padding: '20px' }} className={styles.content} >
-            <span style={{ fontSize: '2.5rem' }}>P</span>
-            <span style={{ fontSize: '2.3rem' }}>R</span>
-            <span style={{ fontSize: '2.1rem' }}>J</span>
-            <span style={{ fontSize: '1.9rem' }}>2</span>
-            <span style={{ fontSize: '1.9rem' }}>F</span>
-            <span style={{ fontSize: '2.1rem' }}>L</span>
-            <span style={{ fontSize: '2.3rem' }}>I</span>
-            <span style={{ fontSize: '2.5rem' }}>X</span>
+        <div style={{ minHeight: '800px', padding: '20px' }}  >
+            <h1 style={{ fontSize: '30px' }}>
+                Cuộc thi tìm hiểu an toàn giao thông cho nụ cười ngày mai
+            </h1>
+            <div>1. truyền thông chăm sóc sức khỏe sinh sản: &nbsp;&nbsp;
+                <a href='https://docs.google.com/presentation/d/1S5MM9AnainQoHh-LuJSFXorm-IieCYPy/edit#slide=id.p1'>Tại đây</a>
+            </div>
+            <div>2. Hỏi đáp về sức khỏe sinh sản: &nbsp;&nbsp;
+                <a href='https://docs.google.com/document/d/1lVFlGE12qPFHeu6kEpIH2spijTlpCAAV/edit#heading=h.gjdgxs'>Tại đây</a>
+            </div>
+            <div>
+                3. PowerPoint bài giảng về sức khỏe sinh sản: &nbsp;&nbsp;
+                <a href='https://drive.google.com/file/d/1aRIZrM0cyHBEWKrqLBNuustLM4LXWLHs/view?usp=sharing'>Tại đây</a>
+            </div>
         </div>
     );
 }
