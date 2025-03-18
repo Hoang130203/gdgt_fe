a2f5b59fcc032f9f2566e797365ebf9ee041a441
Hoang130203 <k58a01.mmh@gmail.com>
2025-03-18 21:19:52 +0700

    .

--- app/documents/page.js
+++ app/documents/page.js
@@ -5,17 +5,33 @@ function Document() {
     return (
         <div style={{ minHeight: '800px', padding: '20px' }}  >
             <h1 style={{ fontSize: '30px' }}>
-                Tài liệu về giáo dục sức khỏe sinh sản
+                Tài liệu về hướng nghiệp
             </h1>
-            <div>1. truyền thông chăm sóc sức khỏe sinh sản: &nbsp;&nbsp;
-                <a href='https://docs.google.com/presentation/d/1S5MM9AnainQoHh-LuJSFXorm-IieCYPy/edit#slide=id.p1'>Tại đây</a>
+            <div>1. Bộ GD& ĐT nêu những điểm mới về về kì thi tốt nghiệp phổ thông 2025: &nbsp;&nbsp;
+                <a href='https://docs.google.com/document/d/1qbKrV_yOQYZTkntZiv_12ACwAGXbk2to/edit'>Tại đây</a>
             </div>
-            <div>2. Hỏi đáp về sức khỏe sinh sản: &nbsp;&nbsp;
-                <a href='https://docs.google.com/document/d/1lVFlGE12qPFHeu6kEpIH2spijTlpCAAV/edit#heading=h.gjdgxs'>Tại đây</a>
+            <div>2. Cẩm nang tuyển sinh đại học năm 2025: &nbsp;&nbsp;
+                <a href='https://docs.google.com/document/d/1eO-Jz4qvzqOg2q7PID2UfCZSKzWFsGD9/edit'>Tại đây</a>
             </div>
             <div>
-                3. PowerPoint bài giảng về sức khỏe sinh sản: &nbsp;&nbsp;
-                <a href='https://drive.google.com/file/d/1aRIZrM0cyHBEWKrqLBNuustLM4LXWLHs/view?usp=sharing'>Tại đây</a>
+                3. Cơ hội việc làm khối ngành khoa học xã hội và sư phạm: &nbsp;&nbsp;
+                <a href='https://docs.google.com/document/d/13T8Nn-lmpZ1k_fl2K_ssi6W-TdlyhHyC/edit'>Tại đây</a>
+            </div>
+            <div>
+                4. Top 5 ngành học đang 'hốt bạc' tại Việt Nam Ra trường có ngay lương 50 triệu tháng: &nbsp;&nbsp;
+                <a href='https://docs.google.com/document/d/13V8ltZcoTkxXLXxOh0Tm3YrPbv3uhqAe/edit'>Tại đây</a>
+            </div>
+            <div>
+                5. Top 7 Trường Đại Học Dễ Xin Việc Nhất Hiện Nay: &nbsp;&nbsp;
+                <a href='https://docs.google.com/document/d/1lc26ysrPB0BLiAC4ApvChAkcUp_5qB6K/edit#heading=h.gjdgxs'>Tại đây</a>
+            </div>
+            <div>
+                6. Top 10+ trường Đại học có học phí thấp nhất Việt Nam: &nbsp;&nbsp;
+                <a href='https://docs.google.com/document/d/1mRvq627jSGjYoGgmET4ZK3emSW4nW3KA/edit#heading=h.gjdgxs'>Tại đây</a>
+            </div>
+            <div>
+                7. Tuyển sinh năm 2025 Nhiều trường đại học dự kiến tăng mạnh học phí: &nbsp;&nbsp;
+                <a href='https://docs.google.com/document/d/1ry3y8YdZW50b4pnQC197El3GOhvb_YhK/edit'>Tại đây</a>
             </div>
         </div>
     );
